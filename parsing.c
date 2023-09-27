42504b48e2a15fa162ab110413f0ba32ee030ee2
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-09-27 17:34:24 +0400

    Add norm to parsing.c

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 11:09:49 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/09/27 17:29:53 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/09/27 17:33:59 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,22 +35,26 @@ static int	validate_args(t_params *params)
 {
 	if (params->num_philosophers > 200)
 	{
-		ft_putstr_fd("❌ Error: number_of_philosophers must be less than 200 ❌ \n", STDERR_FILENO);
+		ft_putstr_fd("❌ Error: number_of_philosophers must be less than \
+200 ❌ \n", STDERR_FILENO);
 		return (1);
 	}
 	if (params->time_to_die < 60)
 	{
-		ft_putstr_fd("❌ Error: time_to_die must be greater than 60 ❌ \n", STDERR_FILENO);
+		ft_putstr_fd("❌ Error: time_to_die must be greater than 60 ❌ \n",
+			STDERR_FILENO);
 		return (1);
 	}
 	if (params->time_to_eat < 60)
 	{
-		ft_putstr_fd("❌ Error: time_to_eat must be greater than 60 ❌ \n", STDERR_FILENO);
+		ft_putstr_fd("❌ Error: time_to_eat must be greater than 60 ❌ \n",
+			STDERR_FILENO);
 		return (1);
 	}
 	if (params->time_to_sleep < 60)
 	{
-		ft_putstr_fd("❌ Error: time_to_sleep must be greater than 60 ❌ \n", STDERR_FILENO);
+		ft_putstr_fd("❌ Error: time_to_sleep must be greater than 60 ❌ \n",
+			STDERR_FILENO);
 		return (1);
 	}
 	return (0);
@@ -65,7 +69,7 @@ int	parse_command_line_args(int argc, char **argv, t_params *params)
 		ft_putstr_fd(" number_of_philosophers time_to_die time_to_eat time_to_sleep \
 [number_of_times_each_philosopher_must_eat]\n", STDERR_FILENO);
 		return (1);
-    }
+	}
 	if (!validate_numeric_args(argc, argv))
 		return (1);
 	params->num_philosophers = ft_atoi(argv[1]);
